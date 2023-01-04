3770eaad188e1ea95bd61fc9d36aca75eaa97d90
InDaRkWeb <96044663+InDaRkWeb@users.noreply.github.com>
2023-01-04 12:04:58 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <h1>MenthaLeau</h1>
+      </div>
+    </div>
+  );
+}
